f10bd94fd4dc920158183f984af63d0d7e227c75
Gauge Garcia <gaugegarcia@my.whccd.edu>
2025-03-02 15:56:12 -0800

    Program Comments Added and FInalization of Code

--- COMP003A.FinalProjectGymManagement/Program.cs
+++ COMP003A.FinalProjectGymManagement/Program.cs
@@ -2,6 +2,9 @@
 {
     internal class Program
     {
+        /// <summary>
+        /// This is where the function runs
+        /// </summary>
         static void Main(string[] args)
         {
             List<Member> members = new List<Member>();
@@ -11,24 +14,29 @@
 
             while (choice != 5)
             {
+                //Validation on our choice Selections
                 try
                 {
+                    //Membership Menu
                     Console.WriteLine("\n Welcome to the Gym Membership Management System!\n Please choose what you would like to execute:");
                     Console.WriteLine(" 1. Add Member.");
                     Console.WriteLine(" 2. Remove Member.");
                     Console.WriteLine(" 3. Display Members");
                     Console.WriteLine(" 4. Exit.");
 
+                    //Choose what to do
                     choice = int.Parse(Console.ReadLine());
 
                     switch (choice)
                     {
+                        //Runs Add Member Method
                         case 1:
                             {
                                 AddMember(members, memberType);
                                 break;
                             }
 
+                        //Identifies Member and Run Remove Member Method
                         case 2:
                             {
                                 Console.WriteLine("Please enter the Id of the member you wish to remove:");
@@ -37,12 +45,14 @@
                                 break;
                             }
 
+                        // Runs Display Member Method
                         case 3:
                             {
                                 DisplayMembers(members);
                                 break;
                             }
 
+                        //Exits the Program
                         case 4:
                             {
                                 choice = 5;
@@ -50,6 +60,7 @@
                                 break;
                             }
 
+                        //Invalidates choice and sets choice to zero to rerun the function.
                         default:
                             {
                                 Console.WriteLine("Invalid input, Please Try Again.");
@@ -58,15 +69,22 @@
                             }
                     }
                 }
-                catch (FormatException)
-                {
-                    Console.WriteLine("Invalid Input!");
-                }
+                    //Catches Format Error
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Invalid Input!");
+                    }
             }
         }
 
+        /// <summary>
+        /// This is the Add Member method, it adds a member with a specific member type.
+        /// </summary>
+        /// <param name="members"></param>
+        /// <param name="memberType"></param>
         static void AddMember(List<Member> members, int memberType)
         {
+            //Validates Member Information
             try
             {
                 Console.Write($"Enter the id of the member: ");
@@ -84,10 +102,11 @@
                 Console.Write($"Enter the age of the member: ");
                 int age = int.Parse(Console.ReadLine());
 
+                //Chooses what kind of member is being added
                 Console.Write($"Enter the type of the member: (1. Powerlifter, 2. Bodybuilder, or 3. Casual) ");
                 memberType = int.Parse(Console.ReadLine());
 
-
+                //Adds Specific member
                 if (memberType == 1)
                 {
                     members.Add(new Powerlifter(id, firstName, middleName, lastName, age));
@@ -108,35 +127,52 @@
                     Console.WriteLine("Member Type not recognized, please try again.");
                 }
             }
+            //Catches Parameter Error
             catch (ArgumentException ex)
             {
                 Console.WriteLine($"Error: {ex.Message}");
             }
+            //Catches Format Error
             catch (FormatException)
             {
                 Console.WriteLine("Invalid Input!");
             }
         }
 
+        /// <summary>
+        /// This is the Remove Member Method. It identifies a member by their id and removes them
+        /// </summary>
+        /// <param name="members"></param>
+        /// <param name="_memberId"></param>
         static void RemoveMember(List<Member> members, string _memberId)
         {
+            //Finds Member based on their ID
             Member member = members.Find(m => m.Id == _memberId);
 
+            //Checks that the Member is found
             if (member == null)
             {
                 Console.WriteLine("Member not found");
             }
             else
             {
+                //Removes Meber from the List
                 members.Remove(member);
                 Console.WriteLine("Member Removed!");
             }
         }
+
+        /// <summary>
+        /// This Is the Display Members Method. It displays the Member info using methods from the member class
+        /// </summary>
+        /// <param name="members"></param>
         static void DisplayMembers(List<Member> members)
         {
-            Console.WriteLine("\nDisplaying all members");
+            Console.WriteLine("\nDisplaying all members:\n");
+            //Loop to display Each member
             foreach (Member member in members)
             {
+                //Display using display function derived in member class.
                 member.DisplayInfo();
             }
         }
